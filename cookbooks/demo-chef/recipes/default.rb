2f334a575b15c63706fb70ac665fc75fb41469a4
keitaroinc <marko.bocevski@keitaro.info>
2015-10-20 19:32:32 +0200

    Added checks to the demo-chef cookbook recipe to make sure that we only do actions if they aren't already there

--- cookbooks/demo-chef/recipes/default.rb
+++ cookbooks/demo-chef/recipes/default.rb
@@ -10,6 +10,7 @@
 group 'demo' do
   action :create
   gid '6111'
+  only_if 'getent group demo'
 end
 
 # we create user 'jsmith' into the provisioned machine
@@ -20,6 +21,7 @@ user 'jsmith' do
   home '/home/jsmith'
   shell '/bin/bash'
   password '$6$3ErpigHW8NcF$2/YxCkxwzCg32mEsuCZkjyNGjCBLd6ktDAbMqpJZAVeKwSA3UG11SbSP7c0voamolr6Q9qNcdDKphrzB3VkPB.'
+  only_if 'getent passwd jsmith'
 end
 
 # and we show the chef equivalent of: mkdir /tmp/demo && chown jsmith:demo /tmp/demo && chmod 755 /tmp/demo 
@@ -28,6 +30,7 @@ directory '/tmp/demo' do
   group 'demo'
   mode '0755'
   action :create
+  not_if { ::File.exist? '/tmp/demo' }
 end
 
 # 2nd example 'Mrs. Smith'
@@ -38,6 +41,7 @@ user 'janesmith' do
   home '/home/janesmith'
   shell '/bin/bash'
   password '$6$3ErpigHW8NcF$2/YxCkxwzCg32mEsuCZkjyNGjCBLd6ktDAbMqpJZAVeKwSA3UG11SbSP7c0voamolr6Q9qNcdDKphrzB3VkPB.jane'
+  only_if 'getent passwd janesmith'
 end
 
 # and we show the chef equivalent of: mkdir /tmp/demo && chown jsmith:demo /tmp/demo && chmod 755 /tmp/demo 
@@ -46,15 +50,18 @@ directory '/tmp/demojane' do
   group 'demo'
   mode '0755'
   action :create
+  not_if { ::File.exist? '/tmp/demojane' }
 end
 
 # and then we show the installation of an rpm, in this case 'tcsh'
 remote_file '/tmp/tcsh-6.18.01-7.el7.x86_64.rpm' do
   source 'http://mirror.centos.org/centos/7/os/x86_64/Packages/tcsh-6.18.01-7.el7.x86_64.rpm'
   mode '0755'
+  not_if { ::File.exist? '/tmp/tcsh-6.18.01-7.el7.x86_64.rpm' }
 end
 
 rpm_package "tcsh" do
-    source "/tmp/tcsh-6.18.01-7.el7.x86_64.rpm"
-    action :install
+  source "/tmp/tcsh-6.18.01-7.el7.x86_64.rpm"
+  action :install
+  not_if "rpm -qa | grep -qx 'tcsh'"  
 end
