5d7f43c67d9d298d87027282bfc9b6abe76580ad
Sys Admin <sysadmin@nobody.example.com>
2015-11-03 18:13:29 +0100

    Import openssh version 1.5.2

---
+++ cookbooks/openssh/metadata.rb
@@ -0,0 +1,22 @@
+name 'openssh'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs openssh'
+version '1.5.2'
+
+recipe 'openssh', 'Installs openssh'
+recipe 'openssh::iptables', 'Set up iptables to allow SSH inbound'
+
+supports 'arch'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'freebsd'
+supports 'redhat'
+supports 'scientific'
+supports 'smartos'
+supports 'suse'
+supports 'ubuntu'
+
+depends 'iptables'
